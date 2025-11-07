2239dac070e65ddc017619f1e2b50875a187de71
Varun Kumar <varunkumar953685@gmail.com>
2025-11-07 12:33:45 +0530

    seo

---
+++ app/sitemap.ts
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://varunkumar.online",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 1.0,
+    },
+  ];
+}
